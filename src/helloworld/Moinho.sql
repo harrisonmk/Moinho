67b111b1f2d17d1964b063186f0cb6cb34d03904
JOAO OTAVIO <JOAO OTAVIO@USER-PC>
2018-06-13 18:22:45 -0400

    Adicionando mais um campo na tabela de participantes.

--- src/helloworld/Moinho.sql
+++ src/helloworld/Moinho.sql
@@ -258,6 +258,9 @@ alter table participantes modify status_participante ENUM('D','M','N_M')
 -- Adicionando um Campo na tabela Participante
 alter table participantes add column idficha INT not null;
 
+-- Adicionando mais um Campo na tabela Participante
+alter table participantes add column idusuario INT not null;
+
 -- Insert na tabela Participantes
 insert into participantes(status_participante,serie,tipo_de_transporte,desempenho,beneficio_social,ano_de_entrada) 
 values('D','Quinto Ano','Bicicleta','Otimo','Nenhum','2014');
